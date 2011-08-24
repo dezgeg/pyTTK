a528d8d7b59a480b928271f6708063c1a64842b5
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-08-24 20:35:16 +0300

    Started working on the Assembler class.

--- pyparser.py
+++ pyparser.py
@@ -22,16 +22,54 @@ operands = register('rj') + Optional(',' + second_operand)
 instruction = opcode('opcode') + Optional(operands)
 data_decl = data_op('opcode') + address('imm_value')
 
-line = Optional(symbol('label')) + (instruction | data_decl)
-line = (line + (LineEnd() | StringEnd())).ignore(';' + restOfLine)
+asm_line = Optional(symbol('label')) + (instruction | data_decl)
+line = (asm_line + (LineEnd() | StringEnd())).ignore(';' + restOfLine).setParseAction(
+		lambda s,l,t: [t])
 
+
+asm_file = ZeroOrMore(line)
 ### glue code ###
-testStr = "add r1,r1 ;asdasd \n foo ds 2 \n sub r1, foo "
-for x in line.scanString(testStr):
-	result = x[0]
-	print testStr[x[1]:x[2]].rstrip()
+class Assembler:
+	def __init__(self, filename, contents):
+		self.program = Program()
+		self.insns = asm_file.parseString(contents)
+		self.data_seg = []
+		self.data_seg_syms = {}
 
-	#print 'Result:', result
+	def assign_data_seg_symbol(self, sym):
+		self.data_seg_syms[sym] = len(data_seg)
+
+	def assign_symbol(self, sym, value):
+		self.program.symbol_table[sym] = value
+
+	def run(self):
+		insns = self.insns
+
+		for i in insns:
+			if i.opcode == 'equ':
+				if not i.label:
+					self.error(i, 'equ without a label')
+				else:
+					self.assign_symbol(i.label, i.imm_value)
+			elif i.opcode == 'dc':
+				self.assign_data_seg_symbol(i.label)
+				self.data_seg.append(i.imm_value)
+			elif i.opcode == 'ds':
+				if i.imm_value < 0:
+					self.error(i, "operand to 'ds' must be positive")
+				else:
+					self.assign_data_seg_symbol(i.label)
+					self.data_seg += (0 for _ in xrange(i.imm_value))
+			else:
+				self.handle_instruction(i)
+
+	def handle_instruction(self, i):
+		if i.opcode != Opcodes.NOP and not i.rj:
+			self.error(i, 'missing first operand')
+			return
+testStr = "add r1,r1 ;asdasd \n ; line comment \n foo ds 2 \n sub r1, foo "
+for result in asm_file.parseString(testStr, True):
+
 	print 'Label:',  result.label
 	print 'Opcode:', result.opcode
 	print 'Rj:', result.rj
